274dc8658bc323ac22e562e7b2cce5de87b9313b
Nguynnn <phamphuckhang24@gmail.com>
2025-07-17 11:44:36 +0700

    add thêm hàm cho blood type với status cho request

--- BloodDonationSystem.DAL/Repositories/BloodTypeRepo/BloodTypeRepo.cs
+++ BloodDonationSystem.DAL/Repositories/BloodTypeRepo/BloodTypeRepo.cs
@@ -7,10 +7,27 @@ namespace BloodDonationSystem.DAL.Repositories.BloodTypeRepo;
 public class BloodTypeRepo : IBloodTypeRepo
 {
     private readonly BloodDonationPrn222Context context;
-    
+
     public async Task<List<BloodType>> GetBloodTypeAsync()
     {
         return await context.BloodTypes.ToListAsync();
     }
 
+    public async Task UpdateBloodTypeAsync(BloodType bloodType)
+    {
+        context.BloodTypes.Update(bloodType);
+        await context.SaveChangesAsync();
+    }
+
+    public async Task CreateBloodTypeAsync(BloodType bloodType)
+    {
+        await context.BloodTypes.AddAsync(bloodType);
+        await context.SaveChangesAsync();
+    }
+
+    public Task DeleteBloodTypeAsync(BloodType bloodType)
+    {
+        context.BloodTypes.Remove(bloodType);
+        return context.SaveChangesAsync();
+    }
 }
--- BloodDonationSystem.DAL/Repositories/BloodTypeRepo/IBloodTypeRepo.cs
+++ BloodDonationSystem.DAL/Repositories/BloodTypeRepo/IBloodTypeRepo.cs
@@ -5,5 +5,7 @@ namespace BloodDonationSystem.DAL.Repositories.BloodTypeRepo;
 public interface IBloodTypeRepo
 {
     Task<List<BloodType>> GetBloodTypeAsync();
-
+    Task UpdateBloodTypeAsync(BloodType bloodType);
+    Task CreateBloodTypeAsync(BloodType bloodType);
+    Task DeleteBloodTypeAsync(BloodType bloodType);
 }
