c3391ef0aca138bfdf144e4d860aff86311a3e1f
Nguynnn <phamphuckhang24@gmail.com>
2025-07-24 08:43:36 +0700

    fix signalR

--- BloodDonationSystem/SignalR.Hubs/NotificationHub.cs
+++ BloodDonationSystem/SignalR.Hubs/NotificationHub.cs
@@ -4,8 +4,5 @@ namespace BloodDonationSystem.SignalR.Hubs;
 
 public class NotificationHub : Hub
 {
-    public async Task SendNotification(string userId, string message)
-    {
-        await Clients.User(userId).SendAsync("ReceiveNotification", message);
-    }
+   
 }
